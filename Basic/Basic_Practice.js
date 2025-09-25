ec56d34ec74a439dd4d428288060352be77fc19c
Jannatul <limajannat1675@gmail.com>
2025-09-26 03:41:35 +0600

    added basic code js problem solving

---
+++ Basic/Basic_Practice.js
@@ -0,0 +1,14 @@
+let name = "Jannaul Lima";
+let age = 22;
+console.log("My Name is ", name, "and I am ", age, "year old.");
+
+let x = 10;
+let y= 2;
+
+console.log("Addition: ", x + y);
+console.log("subtraction : ", x - y);
+console.log("multiplication : ", x * y);
+console.log("divide : ", x / y);
+
+console.log(x > y);
+console.log(x < y);
