a4d5866fe5e6d39465c5b7d8ee9663c2768cac01
Jannatul <limajannat1675@gmail.com>
2025-09-26 09:59:34 +0600

    added basic code js problem solving

--- Basic/Basic_Practice.js
+++ Basic/Basic_Practice.js
@@ -12,3 +12,33 @@ console.log("divide : ", x / y);
 
 console.log(x > y);
 console.log(x < y);
+
+// Problem 1 : Calculate remaining money after buying an apple and an orange
+let my_money = 1000;
+let apple_price = 250;
+let orange_price = 300;
+
+let total_cost = apple_price + orange_price;
+let remaining_money = my_money - total_cost;
+
+console.log("Total Cost: ", total_cost);
+console.log("Remaining Money: ", remaining_money);
+
+// Problem 2 : calculate average of five subjects
+let Mathematics = 75;
+let chemistry = 85;
+let physics = 90;
+let biology = 80;
+let bangla = 70;
+
+let total_marks = Mathematics + chemistry + physics + biology + bangla;
+let average = total_marks / 5;
+
+console.log("Total Marks: ", total_marks);
+console.log("Average Marks: ", average);
+
+// Problem 3 : Reminder of a number when divided by 5
+
+let number = 119;
+let remainder = number % 5;
+console.log("Remainder when ", number, " is divided by 5 is: ", remainder);
